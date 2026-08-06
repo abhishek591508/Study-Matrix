be8d3607ee9c0d120f0671d2a1e414b439e4a27d
Abhishek Kumar <abhishekkumar591508@gmail.com>
2026-08-06 20:15:14 +0530

    Implement update SubSection functionality in controllers

--- controllers/Subsection.js
+++ controllers/Subsection.js
@@ -32,7 +32,11 @@ exports.createSubSection = async (req, res) => {
                                                             subSection:subSectionDetails._id,
                                                         }},
                                                         {new:true});
+
             //HW: log updated section here, after adding populate query
+            const populatedSection = updatedSection.populate("subSection");
+            console.log(populatedSection);
+
             //return response
             return res.status(200).json({
                 succcess:true,
@@ -50,5 +54,53 @@ exports.createSubSection = async (req, res) => {
 };
 
 //HW: updateSubSection
+exports.updateSubSection = async (req,res)=>{
+    try{
+            //get the details
+            const {subSectionId, title, description, timeDuration} = req.body;
+            const video = req.files.videoFile;
+            
+            //validation
+            if(!subSectionId || !title || !description || !timeDuration || !video){
+                return res.status(400).json({
+                    success:true,
+                    message: "All details required"
+                })
+            }
+
+            //upload to cloudinary
+            const uploadedVideo = await uploadImageToCloudinary(video, process.env.FOLDER_NAME);
+            // find by id and update
+            const updatedDetails = await SubSection.findByIdAndUpdate({_id:subSectionId},
+                {
+                    title: title,
+                    description: description,
+                    timeDuration: timeDuration,
+                    videoUrl: uploadedVideo.secure_url
+                },
+                {new:true}
+            )
+            if(!updatedDetails){
+                return res.status(400).json({
+                    success:false,
+                    message: "SubSection not found"
+                })
+            }
+            //return res
+            return res.status(200).json({
+                success:true,
+                message:"Sub section is updated successfully",
+                data: updatedDetails
+            })
+    }
+    catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"Internal server error",
+            error: error.message
+        })
+    }
+
+}
 
 //HW:deleteSubSection
